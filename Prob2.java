59837cc4b3d9bf696bca95832f88624f3aae8383
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-01-01 14:51:30 +0530

    Prob2

---
+++ Prob2.java
@@ -0,0 +1,15 @@
+package Assign3;
+import java.util.Scanner;
+
+public class Prob2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.print(num1+" ");
+        System.out.println(num2);
+    }
+}
